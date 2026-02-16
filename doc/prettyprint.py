6719e309dba361188a871160e8dbec7e66d94724
Karl Anderson <kra@monkey.org>
2026-02-16 12:49:12 -0800

    doc

--- doc/prettyprint.py
+++ doc/prettyprint.py
@@ -1,11 +1,12 @@
 """
 Print metric events from file, one per line.
+grep street-roots out.json | sed "s/.*event': '//"|sed "s/'.*//" |sort|uniq -c | sort -n 
 """
 import glob
 import json
 
-#root = "static-gen/build/date/*/*/*.json"
-root = "data/date/2024/*.json"
+root = "static-gen/build/date/2025/*/*.json"
+#root = "data/date/2024/*.json"
 files = glob.glob(root)
 
 for f in files:
